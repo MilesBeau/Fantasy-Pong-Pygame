f7fe698ba161f1480fc3bada8e868dc9fefdb1b3
MilesBeau <milesb3au@gmail.com>
2025-01-07 18:20:12 +0000

    made ball round

--- Fantasy-Pong-Pygame.py
+++ Fantasy-Pong-Pygame.py
@@ -12,15 +12,19 @@ pygame.display.set_caption("Medieval Pygame Pong")
 # Colours
 white = (255, 255, 255)
 black = (234, 212, 252)
+grey = (191, 191, 191)
 
 # ball 
-ball = pygame.Rect(screen_width/2 - 11.25, screen_height/2 - 11.25,22.5,22.5)
+ball = pygame.Rect(screen_width/2 - 11.25, screen_height/2 - 17.5,35,35)
 
 # Paddle_A
 paddle_A = pygame.Rect(screen_width - 130, screen_height/2 - 75,20,150)
 
 # Paddle_B
 paddle_B = pygame.Rect(screen_width - 1180, screen_height/2 - 75,20,150)
+
+# Score Board
+
 
 
 # Main Game Loop
@@ -29,8 +33,8 @@ while KeepGameRunning:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             KeepGameRunning = False 
-            
-    pygame.draw.rect(screen, white, ball)
+           
+    pygame.draw.ellipse(screen, white, ball)
     pygame.draw.rect(screen, white, paddle_A)
     pygame.draw.rect(screen, white, paddle_B)
     
