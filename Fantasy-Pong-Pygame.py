c783b30e585fa5f99e3a3f163d6a6024ed8be8b8
MilesBeau <milesb3au@gmail.com>
2025-01-09 18:36:41 +0000

    both paddles move with border check

--- Fantasy-Pong-Pygame.py
+++ Fantasy-Pong-Pygame.py
@@ -9,36 +9,49 @@ screen_height = 960
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Medieval Pygame Pong")
 
+# clock
+clock = pygame.time.Clock()
+
 # colours
 white = (255, 255, 255)
-black = (234, 212, 252)
+black = (0, 0, 0)
 grey = (191, 191, 191)
 
 # ball 
 ball = pygame.Rect(screen_width/2 - 11.25, screen_height/2 - 17.5,35,35)
-
-ball_speed_x = 1
-ball_speed_y = 1
+ball_speed_x = 7
+ball_speed_y = 7
 
 # paddle_A
 paddle_A = pygame.Rect(screen_width - 130, screen_height/2 - 75,20,150)
+paddle_A_speed = 0
 
 # paddle_B
 paddle_B = pygame.Rect(screen_width - 1180, screen_height/2 - 75,20,150)
+paddle_B_speed = 0
 
-# Score Board
-
-
-
-# Main-Game-Loop
+# main-Game-Loop
 KeepGameRunning = True
 while KeepGameRunning:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            KeepGameRunning = False 
+            KeepGameRunning = False
+            pygame.quit()
+            sys.exit() 
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                paddle_A_speed=-5.5
+            elif event.key == pygame.K_DOWN:
+                paddle_A_speed =5.5
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_w:
+                paddle_B_speed=-5.5
+            elif event.key == pygame.K_s:
+                paddle_B_speed =5.5
+            
     
     # On-Screen-Objects
-    screen.fill(grey)       
+    screen.fill(black)       
     pygame.draw.ellipse(screen, white, ball)
     pygame.draw.rect(screen, white, paddle_A)
     pygame.draw.rect(screen, white, paddle_B)
@@ -46,15 +59,20 @@ while KeepGameRunning:
     # Movements
     ball.x += ball_speed_x 
     ball.y += ball_speed_y
+    paddle_A.y += paddle_A_speed
+    paddle_B.y += paddle_B_speed
     
     # Border-Checking
     if ball.top <= 0 or ball.bottom >= screen_height:
         ball_speed_y *= -1
     if ball.left <= 0 or ball.right >= screen_width:
         ball_speed_x *= -1
-    
-    pygame.display.flip()
-    
-pygame.quit()
-sys.exit()
+    if paddle_A.top <= 0 or paddle_A.bottom >= screen_height:
+        paddle_A_speed = 0
+    if paddle_B.top <= 0 or paddle_B.bottom >= screen_height:
+        paddle_B_speed = 0
+   
+    pygame.display.update()
+    clock.tick(60)
+
             
